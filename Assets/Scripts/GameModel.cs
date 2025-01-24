8223dc2054ce602eb842c7b6a6ccf1dc92b0fa9f
jobbo994 <jobbo994@gmail.com>
2025-01-24 22:45:39 +0100

    aggiunto model

---
+++ Assets/Scripts/GameModel.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerModel
+{
+    public string Name { get; set; }
+    public string Mail { get; set; }
+    public Score Score { get; set; }
+    public int IdObjective { get; set; }
+}
+
+public class Score
+{
+    public int Horses { get; set; }
+    public int Coppers { get; set; }
+    public int Irons { get; set; }
+    public int Salt { get; set; }
+    public int Wheat { get; set; }
+}
+
+
+public class Objective
+{
+    public int Id { get; set; }
+    public string Description { get; set; }
+    public int? MaxHorses { get; set; }
+    public int? MinHorses { get; set; }
+    public int? MaxCoppers { get; set; }
+    public int? MinCoppers { get; set; }
+    public int? MaxIrons { get; set; }
+    public int? MinIrons { get; set; }
+    public int? MaxSalt { get; set; }
+    public int? MinSalt { get; set; }
+    public int? MaxWheat { get; set; }
+    public int? MinWheat { get; set; }
+
+}
+
+public class GameModel
+{
+    public List<PlayerModel> Players { get; set; }
+}
+
+public List<string> CantoniSvizzeri = new List<string>
+{
+    "Uri", "Svitto", "Untervaldo", "Glarona", "Zugo", "Lucerna", "Zurigo", "Basilea", "Sciaffusa", "Appenzello", "Grisone", "Berna", "Soletta", "Friburgo", "Losanna", "Ginevra", "Neuchâtel", "Vaud", "Valais", "Ticino"
+};
+
+
+
